9a00d1db9310d34c5064666ffb18afdd24e7f518
PabloBlanco10 <pablo_bp94@hotmail.com>
2018-03-29 13:37:38 +0200

    update

---
+++ Codigo/Movies/movies.py
@@ -0,0 +1,66 @@
+########################################################
+#################### TELEGRAM BOT ######################
+################ CINEBOT - @CICINEBOT ##################
+## UCM - MASTER INGENIERIA INFORMATICA - TMI - GRUPO 3 #
+########################################################
+
+##################### AUTORES ##########################
+############## Andres Aguirre Juarez ###################
+############### Pablo Blanco Peris #####################
+############# Maria Castañeda Lopez ####################
+############### Maurizio Vittorini #####################
+########################################################
+
+
+
+import sys
+import time
+import imdb
+
+
+class Movie:
+
+    def __init__(self):
+        # Create the object that will be used to access the IMDb's database.
+        self.ia = imdb.IMDb() # by default access the web.
+
+        self.searchmovie()
+        self.getinfofrommovie()
+        self.getmovie()
+        # self.data = []
+
+    def searchmovie(self):
+        # Search for a movie (get a list of Movie objects).
+        movie = 'Black Panther'
+        self.result = self.ia.search_movie(movie)
+
+
+    def getinfofrommovie(self):
+        # get first result
+        self.the_unt = self.result[0]
+        self.ia.update(self.the_unt)
+
+
+    # Print some information.
+    def getmovie(self):
+        self.searchmovie()
+        self.getinfofrommovie()
+
+        duracion = self.the_unt['runtime']
+        valoracion = self.the_unt['rating']
+        director = self.the_unt['director'] # get a list of Person objects.
+        sinopsis = self.the_unt['plot outline']
+        generos = self.the_unt['genres']
+        urlcartel = self.the_unt['cover url']
+        actores = self.the_unt['cast']
+        actores = actores[0:10]
+        stringActores = ''
+        for actor in actores:
+            stringActores += actor['name']+', '
+
+        print("Duracion: " + duracion[0])
+        print("Valoración: " + str(valoracion))
+        print("Director: " + director[0]['name'])
+        print("Géneros: " + str(generos))
+        print("Actores: " + stringActores)
+        print("Sinopsis: " + sinopsis)
--- Codigo/cinebot.py
+++ Codigo/cinebot.py
@@ -14,15 +14,20 @@
 
 import sys
 import time
+
+from pytube import YouTube
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+import Codigo.Movies.movies as Movies
 
-
-TOKEN = '556801610:AAEDqKjjIZkWCJzARY_DwwIHzBoGjCImKZM'  # @Cicinebot
+# TOKEN = '556801610:AAEDqKjjIZkWCJzARY_DwwIHzBoGjCImKZM'  # @Cicinebot
 TOKEN = '581607975:AAG995XceTIs5DjdW70blkjF3__IGCKv2_w'  # @CicinebotPablo_bot
-TOKEN = '574044701:AAHVro7hwe2YQ-VHXcXb5cVQJP1CYxyo5AE'  # @CicinebotMaria_bot
-TOKEN = '551454537:AAHZ_VFOqHqQO0lLMGtzJi0XsCYo5cCxvrM'  # @CicinebotMaurizio_bot
+# TOKEN = '574044701:AAHVro7hwe2YQ-VHXcXb5cVQJP1CYxyo5AE'  # @CicinebotMaria_bot
+# TOKEN = '551454537:AAHZ_VFOqHqQO0lLMGtzJi0XsCYo5cCxvrM'  # @CicinebotMaurizio_bot
+
+bot = telepot.Bot(TOKEN)
+
 
 #funcion que gestiona los mensajes recibidos por el chat
 def on_chat_message(msg):
@@ -32,6 +37,24 @@ def on_chat_message(msg):
 
     if mensaje == '/start':
         bot.sendMessage(chat_id, 'Buenas, soy CineBot, ¿te apetece ir al cine?')
+
+    if mensaje == 'video':
+        link = "http://www.youtube.com/watch?v=xWOoBJUqlbI"
+        #
+        # try:
+        #     # object creation using YouTube which was imported in the beginning
+        #     yt = YouTube(link)
+        # except:
+        #     print("Connection Error")  # to handle exception
+        #
+        # # get the video with the extension and resolution passed in the get() function
+        # d_video = yt.get(mp4files[-1].extension, mp4files[-1].resolution)
+        #
+        # # filters out all the files with "mp4" extension
+        # mp4files = yt.filter('mp4')
+
+        bot.sendVideo(chat_id, 'https://www.youtube.com/watch?v=xWOoBJUqlbI')
+
     else:
         bot.sendMessage(chat_id, mensaje)
         keyboard = InlineKeyboardMarkup(inline_keyboard=[
@@ -50,13 +73,18 @@ def on_callback_query(msg):
     bot.answerCallbackQuery(query_id, text='Te he pillado y lo sabes')
 
 
-bot = telepot.Bot(TOKEN)
+def main():
 
-MessageLoop(bot, {'chat': on_chat_message,
-                  # callback_query es el boton del chat
-                  'callback_query': on_callback_query}).run_as_thread()
-print('Listening ...')
+    # test with imdb library
+    movie = Movies.Movie()
 
-while 1:
-    time.sleep(10)
+    MessageLoop(bot, {'chat': on_chat_message,
+                      # callback_query es el boton del chat
+                      'callback_query': on_callback_query}).run_as_thread()
+    print('Listening ...')
 
+    while 1:
+        time.sleep(10)
+
+
+main()
